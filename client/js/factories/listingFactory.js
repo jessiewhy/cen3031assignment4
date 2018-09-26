077d2cc2f75a92f67d8ab47b91235f1da0f95545
Your Name here <your_email@domainname.com>
2018-09-25 22:36:19 -0400

    added styling

--- client/js/factories/listingFactory.js
+++ client/js/factories/listingFactory.js
@@ -15,4 +15,5 @@ angular.module('listings', []).factory('Listings', function($http) {
       return $http.delete('http://localhost:8080/api/listings', id);
     }
   }
+  return methods;
 })
--- server/config/config.js
+++ server/config/config.js
@@ -2,5 +2,5 @@ module.exports = {
     db: {
       uri: 'mongodb://CEN3031:CEN3031TA@ds159772.mlab.com:59772/jessiedb', //place the URI of your mongo database here.
     }, 
-    port: 8080
+    port: (process.env.PORT || 8080)
   };
